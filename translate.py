c224b8a79c142247ef99589cc4b02f445717204e
Thomas Albertine <albertit@onid.oregonstate.edu>
2016-03-08 16:11:56 -0800

    Partial translate.py

--- translate.py
+++ translate.py
@@ -1,10 +1,16 @@
 from ast import *
+import sys
 
 def getNewIdNum():
     retval = getNewIdNum.nextNum
     getNewIdNum.nextNum += 1
     return retval
 getNewIdNum.nextNum = 0
+
+class Var:
+  def __init__(self, name):
+    self.name = name
+    self.id = getNewIdNum
 
 def generate_c(n, dec_vars):
     if isinstance(n, Module):
@@ -17,21 +23,44 @@ def generate_c(n, dec_vars):
                           "int main()",
                           "{", 
                           variableDeclaration,
-                          "True__char = 1;\nFalse__char = 0;",
                           "\n".join(genList), 
                           "return 0;",
                           "}"])
-    #Type literals
-    
+    elif isinstance(n, Print) #Print Statements
+      retval = "pyobjPrint(vector<pyobj *>(["
+      values = []
+      for i in n.values:
+        values.append(generate_c(i, dec_vars))
+      values = ", ".join(values)
+      retval += values
+      retval += "], "
+      if n.nl:
+        retval += 'true'
+      else:
+        retval += 'false'
+      retval += ');'
+      return retval
+
+    #loops
+    elif isinstance(n, While):
+      retval = "while (pyobjToBool(" + generate_c(n.test) + ")){\n"
+      body = []
+      for i in n.body:
+        body.append(generate_c(i))
+      retval += "\n".join(body)
+      retval += "}"
+    elif isinstance(n, Assign):
+      
+
+
     else:
         raise sys.exit('Error in generate_c: unrecognized AST node: %s' % n)
 
 if __name__ == "__main__":
 
     try:
-        mylexer = MyLexer()
-        mylexer.input(sys.stdin.read())
-        tree = parse_module()
+        source = sys.stdin.read()
+        tree = parse(source)
 
         dec_vars = set()
 
