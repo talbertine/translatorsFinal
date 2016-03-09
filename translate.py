60d22c0a150b97c1f1834c70255cf6132793b27f
Thomas Albertine <albertit@onid.oregonstate.edu>
2016-03-08 18:10:43 -0800

    I've gotten to the for loop. I'm breaking for dinner, and then I'll get back to it.

--- translate.py
+++ translate.py
@@ -48,9 +48,51 @@ def generate_c(n, dec_vars):
       for i in n.body:
         body.append(generate_c(i))
       retval += "\n".join(body)
-      retval += "}"
+      retval += "}\n"
+      return retval
+
+    elif isinstance(n, For):
+      iterVar = "i_" + str(getNewIdNum())
+      retval = "for ( int " + iterVar " = 0; pyobjGetLen(pyobjGetItr(" + generate_c(n.iter) + ")) > " + iterVar + "; ++" + iterVar + "){\n"
+      retval += "pyobjDecRef(pyobjAssign(&" + generate_c(target) + ", pyobjIndex(pyobjGetItr(" + generate_c(n.iter) + "), pyobjInt(" + iterVar + "))));\n"
+      body = []
+      for i in n.body:
+        body.append(generate_c(i))
+      retval += "\n".join(body)
+      retval += "}\n"
+      return retval
+    #Assignment statement
+    #This one's special because we have to wait until after the assignments to 
+    # decrement the reference count, so the value is not deleted prematurely
     elif isinstance(n, Assign):
-      
+      retval = "pyobjDecRef("
+      stack = []
+      for i in n.targets:
+        stack.append(')')
+        retval += "pyobjAssign(&" + generate_c(i) + ", "
+      retval += generate_c(n.value)
+      retval += "".join(stack) + ");\n"
+      return retval
+
+    #if
+    elif isinstance(n, If):
+      retval = "if (pyobjToBool(" + generate_c(n.test) + ")){\n"
+      body = []
+      for i in n.body:
+        body.append(generate_c(i))
+      retval += "\n".join(body)
+      retval += '}\n'
+      if len(n.orelse) > 0:
+        retval += "else {\n"
+        body = []
+        for i in n.orelse:
+          body.append(generate_c(i))
+        retval += "\n".join(body)
+        retval += '}\n'
+      return retval
+
+
+
 
 
     else:
